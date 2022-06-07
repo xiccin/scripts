9c753b3b6c2a44838cf53672c816b681471fea23
xiccin <su@xicc.in>
2022-06-07 19:31:11 +0530

    modified:   push-fragmentor.sh

--- push-fragmentor.sh
+++ push-fragmentor.sh
@@ -1,15 +1,31 @@
 #!/bin/bash
 
+tput civis
 folder_location="data/fastdl/maps"
 
 # discard un-pushed commits 
-git reset HEAD~1 >& /dev/null
+git reset HEAD~1
+
+mkdir frag-cache
+git ls-files > frag-cache/pushed.list
 
 for filename in $folder_location/*
 do
-    clear
-    git add $filename
-    git commit -m "maps fragment"
-    git push
+    if grep -qF "$filename" frag-cache/pushed.list
+    then
+        continue
+    else
+        clear
+        echo -e "pushing: $filename \n"
+        git add $filename
+        git commit -m "maps fragment"
+        echo "" 
+        git push -f
+    fi    
 done
 
+git add $folder_location
+git commit -m "maps fragment"
+git push -f
+
+rm -r frag-cache
